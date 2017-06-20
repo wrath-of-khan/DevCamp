6c3d1bbdc7eaf80e0cb3efb65c2195e5e3bbbf84
Jordan Hudgens <jordan@devcamp.com>
2017-06-20 15:11:37 +0000

    more page_controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,10 +1,7 @@
 class PagesController < ApplicationController
   def home
-<<<<<<< HEAD
     @posts = Blog.all
     @skills = Skill.all
-=======
->>>>>>> 3f292d680caaf59e6e6d48fa33904a6065694813
   end
 
   def about
